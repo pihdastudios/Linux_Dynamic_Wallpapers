16a00deb71b3df2dac6d4398f0b1675b3e0882dc
A.d. I B <62319714+pihdastudios@users.noreply.github.com>
2024-11-04 15:14:07 +0700

    :beetle: Fix permissions from 700 to 755
    The settings app cannot access the wallpaper and gives a blank image. This should fix it.

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ echo "Installing wallpapers..."
 sudo mkdir -p /usr/share/backgrounds/
 sudo mkdir -p /usr/share/gnome-background-properties/ 
 sudo cp -r $(pwd)/Dynamic_Wallpapers /usr/share/backgrounds/Dynamic_Wallpapers
+sudo chmod -R 755 /usr/share/backgrounds/Dynamic_Wallpapers
 sudo cp $(pwd)/xml/* /usr/share/gnome-background-properties/
 echo "Wallpapers has been installed. Enjoy setting them as your desktop background!"
